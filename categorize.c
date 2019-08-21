d1587de2f50a1be9d2462fed36d121114f74f4c7
张鑫强 <1933581361@qq.com>
2019-08-21 17:30:35 +0800

    提交代码 categorize.c 有错误

---
+++ categorize.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+	char line[80];
+	FILE *in = fopen("spooky.csv","r");
+	FILE *file1 = fopen("ufos.csv","w") ;
+	FILE *file2 = fopen("disappearances.csv","w");
+	FILE *file3 = fopen("other.csv","w");
+	
+	while(fscanf(in,"%79[^\n]\n", line) == 1) {
+		if (strstr (line, "UFO"))
+			fprintf(filel, "%s\n", line) ;
+		else if (strstr(line, "Disappearance"))
+			fprintf(file2,"%s\n",line) ;
+		else
+			fprintf(file3,"%s\n", line) ;
+	}
+	fclose(file1);
+	fclose(file2);
+	fclose(file3);
+	return 0;
+}
+
