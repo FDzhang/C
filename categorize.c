7c0dc94a376a3febfd5b759f94e88f414e68ed33
张鑫强 <1933581361@qq.com>
2019-08-26 10:03:05 +0800

    改进cotegorize.c 添加order_pizza.c以学习unistd.h中的getopt()

--- categorize.c
+++ categorize.c
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){ 
+int main(int argc, char *argv[]){ 
 	char line[80];
+	
+	if(argc!=6){
+		fprintf(stderr,"You need to give 5 arguments\n");
+		return 1;
+	} 
 	FILE *in = fopen("spooky.csv","r");
-	FILE *file1 = fopen("ufos.csv","w") ;
-	FILE *file2 = fopen("disappearances.csv","w");
-	FILE *file3 = fopen("other.csv","w");
+	FILE *file1 = fopen(argv[2],"w") ;
+	FILE *file2 = fopen(argv[4],"w");
+	FILE *file3 = fopen(argv[5],"w");
 	
 	while(fscanf(in,"%79[^\n]\n", line) == 1) {
-		if (strstr (line, "UFO"))
+		if (strstr (line, argv[1]))
 			fprintf(file1, "%s\n", line) ;
-		else if (strstr(line, "Disappearance"))
+		else if (strstr(line, argv[3]))
 			fprintf(file2,"%s\n",line) ;
 		else
 			fprintf(file3,"%s\n", line) ;
