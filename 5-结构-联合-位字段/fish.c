3193c70b68f218d392ba6bcda0e93d67ad26c696
张鑫强 <1933581361@qq.com>
2019-09-16 09:07:59 +0800

    5结构与联合

---
+++ 5-结构-联合-位字段/fish.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+struct fish {
+	const char *name ;
+	const char *species;
+	int teeth;
+	int age;
+};
+
+
+/*打印目录项*/
+void catalog (struct fish f)
+{
+	printf ("%s is a %s with %i teeth. He is %i\n", 
+			f.name, f.species,f.teeth, f.age) ;
+}
+/*打印贴在水缸上的标签*/
+void label (struct fish f)
+{
+	printf ("Name:%s \nSpecies:%s\n%i years old,%i teeth\n", 
+			f.name, f.species,f.teeth, f.age);
+}
+
+int main(){
+	
+	struct fish snappy = { "Snappy", "Piranha", 69, 4} ;
+	catalog (snappy) ;
+	label (snappy) ;
+
+	return 0;
+}
+
